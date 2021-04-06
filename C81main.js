7d2fc9218289afa55972929748aab70487b07472
rrs12 <77575080+rrs12@users.noreply.github.com>
2021-04-06 14:27:58 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,33 @@
+var canvas= document.getElementById("myCanvas");
+var ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle="#0085C7";
+ctx.lineWidth=5;
+ctx.arc(200,200,40,0,2* Math.PI)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="black";
+ctx.lineWidth=5;
+ctx.arc(300,200,40,0,2* Math.PI)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="#DF0024";
+ctx.lineWidth=5;
+ctx.arc(400,200,40,0,2* Math.PI)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="#F4C300";
+ctx.lineWidth=5;
+ctx.arc(250,240,40,0,2* Math.PI)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle="#009F3D";
+ctx.lineWidth=5;
+ctx.arc(350,240,40,0,2* Math.PI)
+ctx.stroke();
+
