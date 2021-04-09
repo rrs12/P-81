396b8b23b810fa86c50c81023036b639498f7931
rrs12 <77575080+rrs12@users.noreply.github.com>
2021-04-09 12:57:01 +0530

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -29,5 +29,11 @@ ctx.beginPath();
 ctx.strokeStyle="#009F3D";
 ctx.lineWidth=5;
 ctx.arc(350,240,40,0,2* Math.PI)
+ctx.rect(150, 143, 300, 150);
 ctx.stroke();
 
+ctx.beginPath();
+ctx.strokeStyle="grey";
+ctx.lineWidth=5;
+ctx.rect(150, 143, 300, 150);
+ctx.stroke();
